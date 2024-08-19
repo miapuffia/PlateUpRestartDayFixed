d322a60dad8750f951355760fb25799a68632a1e
Robert D. Rioja (miapuffia) <miapuffia@gmail.com>
2024-08-19 19:47:49 -0400

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using KitchenMods;
+using System.Collections.Generic;
+using System.Reflection;
+
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenRestartDayFixed {
+	public class Main : IModInitializer {
+		public const string MOD_ID = "miapuffia.PlateUp.RestartDayFixed";
+		public const string MOD_NAME = "Restart Day Fixed";
+		public const string MOD_VERSION = "1.0.0";
+
+		public void PostActivate(Mod mod) {
+			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), MOD_ID);
+		}
+
+		public void PreInject() { }
+
+		public void PostInject() { }
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PlateUpRestartDayFixed
+ Fixed version of Mayple Syrup ^_^'s Restart Day (Soft Opening) mod.
